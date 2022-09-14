e71f2f587bc40d01c85452f1214fc299f64555f3
psngoc <psngoc2000@gmail.com>
2022-09-14 06:58:18 +0000

    dd

--- config/mail.php
+++ config/mail.php
@@ -42,6 +42,7 @@ return [
             'username' => env('MAIL_USERNAME'),
             'password' => env('MAIL_PASSWORD'),
             'timeout' => null,
+	    'sendmail' => '/usr/sbin/sendmail -bs',
             'auth_mode' => null,
         ],
 
