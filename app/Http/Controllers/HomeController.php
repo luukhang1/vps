6553bab09e2096f20e3fdc2710babfeb5f778a82
ngoc.psh <ngoc.psh@kiotviet.com>
2022-09-14 16:47:56 +0700

    dd

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Auth;
+
 class HomeController extends Controller
 {
     public function index()
@@ -11,6 +13,7 @@ class HomeController extends Controller
 
     public function login()
     {
+        Auth::logout();
         return view('user.login');
     }
 
